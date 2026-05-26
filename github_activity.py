904b0a90403d1c86683d04de3c8a97bce2128273
cristhyan-n <cristhyan.vo@gmail.com>
2026-05-26 17:57:24 -0300

    feat: update github_activity.py to show events

--- github_activity.py
+++ github_activity.py
@@ -1,6 +1,10 @@
 import sys
+from api import get_activity
+from formatter import display_activity
 
 if len(sys.argv) < 2:
     print("Insira um argumento válido.")
     sys.exit(1)
-print(sys.argv[1])
+
+get_event = get_activity(sys.argv[1])
+display_activity(get_event)
