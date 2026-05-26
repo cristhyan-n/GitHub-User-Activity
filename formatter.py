6c2baba27388071abf58b04916d0afe1ff927eff
cristhyan-n <cristhyan.vo@gmail.com>
2026-05-26 19:29:41 -0300

    feat: update formatter.py pushes

--- formatter.py
+++ formatter.py
@@ -1,4 +1,22 @@
 
 def display_activity(events):
     for event in events :
-        print(f"{event['type']} - {event['repo']['name']}")
+        if event["type"] == "PushEvent":
+            print(f"Pushed to {event['repo']['name']}")
+            
+        elif event["type"] == "IssuesEvent":
+            print(f"Opened an issue in {event['repo']['name']}")
+
+        elif event["type"] == "WatchEvent":
+            print(f"Starred {event['repo']['name']}")
+
+        elif event["type"] == "ForkEvent":
+            print(f"Forked {event['repo']['name']}")
+
+        elif event["type"] == "CreateEvent":
+            print(f"Created {event['repo']['name']}")
+
+        elif event["type"] == "PullRequestEvent":
+            print(f"Opened a pull request in {event['repo']['name']}")
+        else:
+            print(f"Unknown event: {event['type']} in {event['repo']['name']}")
